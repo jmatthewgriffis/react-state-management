2ac4c2d701459ee782489575b035cf8117b0fc97
J. Matthew Griffis <matticgreek@gmail.com>
2020-10-04 09:23:33 -0600

    Add button to decrement.

--- src/components/Book/Book.tsx
+++ src/components/Book/Book.tsx
@@ -21,12 +21,21 @@ export const Book = ({
     return 0;
   });
 
+  const decrement = (): void => setTimesRead(timesRead - 1);
   const increment = (): void => setTimesRead(timesRead + 1);
 
   return (
     <div>
-      <span className="underline">{title}</span> by {author} | Times read:{' '}
-      {timesRead}
+      <span className="underline">{title}</span> by {author}
+      <button
+        type="button"
+        disabled={!timesRead}
+        onClick={decrement}
+        className="margin-sides"
+      >
+        I didn't read it
+      </button>
+      Times read: {timesRead}
       <button type="button" onClick={increment} className="margin-sides">
         I read it
       </button>
