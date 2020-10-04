b8b8a79438b7ec1257917705251a4a059114cea9
J. Matthew Griffis <matticgreek@gmail.com>
2020-10-03 22:19:54 -0600

    Use `useState` React Hook.

--- src/components/Book/Book.tsx
+++ src/components/Book/Book.tsx
@@ -7,8 +7,23 @@ export const Book = ({
 }: {
   title: string;
   author?: string;
-}): ReactElement => (
-  <div>
-    <span className="underline">{title}</span> by {author}
-  </div>
-);
+}): ReactElement => {
+  const [timesRead, setTimesRead] = React.useState((): number => {
+    console.log(
+      `Using lazy init for '${title}', so this should only fire once.`
+    );
+    return 0;
+  });
+
+  const increment = (): void => setTimesRead(timesRead + 1);
+
+  return (
+    <div>
+      <span className="underline">{title}</span> by {author} | Times read:{' '}
+      {timesRead}
+      <button type="button" onClick={increment} className="margin-left">
+        I read it
+      </button>
+    </div>
+  );
+};
